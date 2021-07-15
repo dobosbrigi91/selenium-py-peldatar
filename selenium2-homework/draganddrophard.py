e0e5e3a54356ef4ee44a70c5f539a352feaf2ab2
Dobos Brigi <dobosbrigi91@gmail.com>
2021-07-15 06:34:04 +0200

    021 Feladat: Lapozó kontrol feladatok

---
+++ selenium2-homework/draganddrophard.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from os import getcwd
+import time
+
+try:
+    driver = webdriver.Chrome()
+    driver.get('https://gentle-bay-0e4e13803.azurestaticapps.net/dragndrop2.html')
+
+    time.sleep(3)
+
+    cwd = getcwd()
+
+    js_drag_drop = open(cwd + '\\dnd.js', 'r').read()
+
+    items = driver.find_elements_by_xpath('//*[@id="Todo"]/li')
+    target = driver.find_element_by_xpath('//*[@id="Doing"]')
+
+    for item in items:
+        driver.execute_script(js_drag_drop, item, target)
+
+finally:
+    driver.close()
